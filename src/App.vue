a56942eb5543cfeb194169f4929f2290ddf2ccf7
李元芳 <jun.zheng@ideacome.com>
2020-11-21 13:04:15 +0800

    [feature] 拼图游戏

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Puzzle/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Puzzle from './components/Puzzle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Puzzle
   }
 }
 </script>
---
+++ src/components/Puzzle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="info">
+      <p class="tips">点击要移动的模块</p>
+      <div class="tips">步数：<span id="step">0</span></div>
+    </div>
+    <div id="main">
+      <div id="block_0" class="block"><span class="order">1</span></div>
+      <div id="block_1" class="block"><span class="order">2</span></div>
+      <div id="block_2" class="block"><span class="order">3</span></div>
+      <div id="block_3" class="block"><span class="order">4</span></div>
+      <div id="block_4" class="block"><span class="order">5</span></div>
+      <div id="block_5" class="block"><span class="order">6</span></div>
+      <div id="block_6" class="block"><span class="order">7</span></div>
+      <div id="block_7" class="block"><span class="order">8</span></div>
+      <div id="block_8" class="block"><span class="order">9</span></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Puzzle",
+};
+</script>
